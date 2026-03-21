adab9c7ccbf44e453dcfd709b32da718e5a9eb1d
Burhanuddin Kanchwala <dotcomsolutiononline@gmail.com>
2026-03-21 23:36:06 +0530

    ...

--- inc_files/mobile_sidebar.php
+++ inc_files/mobile_sidebar.php
@@ -1,12 +1,12 @@
 <div class="mobile-menu-wrapper">
     <span class="mobile-menu-close"><i class="fa fa-times"></i></span>
-    <nav class="mobile-nav">
-        <ul class="mobile-menu">
-            <li>
-                <a href="https://haneri.com">Home</a>
+    <nav class="mobile-nav haneri-mobile-nav">
+        <ul class="mobile-menu haneri-mobile-menu list-none p-0">
+            <li class="haneri-mobile-nav-item">
+                <a class="haneri-mobile-nav-link" href="https://haneri.com/">Home</a>
             </li>
-            <li>
-                <a href="https://haneri.com/shop">Categories</a>
+            <li class="haneri-mobile-nav-item">
+                <a class="haneri-mobile-nav-link" href="https://haneri.com/shop">Categories</a>
                 <ul>
                     <li><a href="https://haneri.com/shop">Ceiling Fan</a></li>
                     <li><a href="https://haneri.com/shop">Table Wall Pedestals</a></li>
@@ -14,47 +14,28 @@
                     <li><a href="https://haneri.com/shop">Personal</a></li>
                 </ul>
             </li>
-            <li>
-                <a href="https://haneri.com/air-curve-design">Pillar Technology</a>
+            <li class="haneri-mobile-nav-item">
+                <a class="haneri-mobile-nav-link" href="https://haneri.com/air-curve-design">Pillar Technology</a>
                 <ul>
                     <li><a href="https://haneri.com/air-curve-design">Air Curve Design</a></li>
                     <li><a href="https://haneri.com/turbosilent-bldc">TurboSilent BLDC</a></li>
-                    <li><a href="hhttps://haneri.com/hass">HASS</a></li>
+                    <li><a href="https://haneri.com/hass">HASS</a></li>
                     <li><a href="https://haneri.com/lumiambience">LumiAmbience</a></li>
                     <li><a href="https://haneri.com/scan">SCAN</a></li>
                 </ul>
             </li>
-            <li>
-                <a href="https://haneri.com/our-story">About Us</a>
+            <li class="haneri-mobile-nav-item">
+                <a class="haneri-mobile-nav-link" href="https://haneri.com/our-story">About Us</a>
                 <ul>
-                    <li><a href="https://haneri.com/our-story#vision">VISION</a></li>
-                    <li><a href="https://haneri.com/our-story#mission">MISSION</a></li>
-                    <li><a href="https://haneri.com/our-story#values">VALUES</a></li>
+                    <li><a href="https://haneri.com/our-story#vision">Vision</a></li>
+                    <li><a href="https://haneri.com/our-story#mission">Mission</a></li>
+                    <li><a href="https://haneri.com/our-story#values">Values</a></li>
                     <li><a href="https://haneri.com/our-brands#haneri">Our Brands</a></li>
-                    <!-- <li><a href="capabilities.php">Capabilities</a></li> -->
                 </ul>
             </li>
-
-            <li>
-                <a href="https://haneri.com/contact">Contact Us</a>
-                <!-- <ul>
-                    <li><a href="#">FAQs</a></li>
-                    <li><a href="#">Videos</a></li>
-                    <li><a href="#">Catalogues</a></li>
-                    <li><a href="#">Manuals</a></li>
-                </ul>
-                <a href="our_brands.php">Corporate Enquiry</a>
-                <ul>
-                    <li><a href="#">FORM</a></li>
-                </ul>
-                <a href="contact.php">Contact Us</a> -->
+            <li class="haneri-mobile-nav-item">
+                <a class="haneri-mobile-nav-link" href="https://haneri.com/contact">Contact Us</a>
             </li>
         </ul>
     </nav>
-    <!-- End .mobile-nav -->
-
-    <!-- <form class="search-wrapper mb-2" action="#">
-        <input type="text" class="form-control mb-0" placeholder="Search..." required />
-        <button class="btn icon-search text-white bg-transparent p-0" type="submit"></button>
-    </form> -->
 </div><!-- End .mobile-menu-wrapper -->
